6857f0c2ef7bac9c0b6ebbd5b5ae275a97c865d7
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-06-02 07:42:49 -0700

    Update ccache.sh

--- ccache.sh
+++ ccache.sh
@@ -6,7 +6,7 @@ nproc
 cat /etc/os*
 env
 
-url=https://gdrive.peersalik16.workers.dev/0:/ccache/ccache-ci/ccache.tar.gz
+url=https://yellow-butterfly-09f4.sundramsayss.workers.dev/ccache/
 
 cd /tmp
 time aria2c $url -x16 -s50
